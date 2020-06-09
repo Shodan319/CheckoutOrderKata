57126a6f67e8ff68a7a5699cd4bdb8cca6ff57f0
JETM319 <jetmeans@gmail.com>
2020-06-09 17:13:03 -0400

    Initial commit

---
+++ src/test/java/TestCheckoutOrder.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestCheckoutOrder
+{
+    @Test
+    public void validateSetup()
+    {
+        var expected = 1;
+        assertEquals(expected, 2);
+    }
+}
